a15c1c06c470aca16c3b5f27ac62d92e5d217804
anasanmua <86674956+anasanmua@users.noreply.github.com>
2021-11-25 18:08:14 +0100

    fixing img margin and position

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -26,7 +26,7 @@ export default function Dictionary(props) {
 
     let pexelsApiKey =
       "563492ad6f91700001000001e8be9e253360448d8d6983955a16c627";
-    let pexelsApiUrl = `https://api.pexels.com/v1/search?query=${keyword}&per_page=5`;
+    let pexelsApiUrl = `https://api.pexels.com/v1/search?query=${keyword}&per_page=6`;
     axios
       .get(pexelsApiUrl, {
         headers: { Authorization: `Bearer ${pexelsApiKey}` },
--- src/Photos.js
+++ src/Photos.js
@@ -1,12 +1,12 @@
 import React from "react";
+import "./Photos.css";
 
 export default function Photos(props) {
-  if (props.photo) {
+  if (props.photos) {
     return (
       <section className="Photos">
-        Hello
         <div className="row">
-          {props.photo.map(function (photo, index) {
+          {props.photos.map(function (photo, index) {
             return (
               <div className="col-4" key={index}>
                 <a href={photo.src.original} target="_blank" rel="noreferrer">
